8753c726ed0d16c394af95d03aee857e3a0ef9a6
Berkcan Gümüşışık <75336900+berkcangumusisik@users.noreply.github.com>
2022-04-12 08:34:30 +0300

    Day 7 finished

---
+++ Day 7/03.listelerinDongulerleKullanimi.dart
@@ -0,0 +1,10 @@
+void main(List<String> args) {
+  List<String> meyveler = ["Çilek","Muz","Elma","Kivi","Kiraz"];
+  for (var meyve in meyveler) {
+    print(meyve);
+  }
+  print("-----------------------------");
+  for (var i = 0; i < meyveler.length; i++) {
+    print(meyveler[i]);
+  }
+}
